fe566cc6763aa2ce78e462ed5f5fb6cf24a475bc
Ronyson <59067125+ronysonSilva@users.noreply.github.com>
2021-04-17 16:18:51 -0300

    atualização de classe e adição de grilos

--- grilos/src/Main.java
+++ grilos/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	static int posicao = 0;
+	static int posicao, gg, totalpulos1, totalpulos2, totaldist1, totaldist2, timeW = 0;
 	static int disMax; 
 	
 	
@@ -14,17 +14,23 @@ public class Main {
 		
 		System.out.print("Quantos grilos deseja? ");
 	    int Qgrilos = scanner.nextInt();
-	    Qgrilos += 1;
+	    while(Qgrilos > 30) {
+	    	System.out.print("30 é o limite! Digite novamente...");
+	    	Qgrilos = scanner.nextInt();
+	    }
+	    
+	    gg = Qgrilos + 1;
+	    posicao +=1;
 	    System.out.print("Qual Distancia da linha de chegada? ");
 	    disMax = scanner.nextInt();
 	
-	    Grilo []g = new Grilo[Qgrilos];
+	     Grilo []g = new Grilo[Qgrilos];
 	    
 	    for(int i = 1; i<Qgrilos; i++) {
 	    	
 	    	g[i] = new Grilo (i);
 	    	g[i].start();
-	    }
+	    
 		
 	
 	
